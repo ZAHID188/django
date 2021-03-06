1586a08527401a80829c792e2f445a6c9ded9f3f
zahid188 <zahidpranta5@gmail.com>
2021-03-06 09:21:42 -0500

    airline project

--- project1/newyear/views.py
+++ project1/newyear/views.py
@@ -4,6 +4,7 @@ import datetime
 # Create your views here.
 def index(request):
     now= datetime.datetime.now()
+    #render a template ,template name-"newyear/index.html
     return render(request,"newyear/index.html",{
         "newyear" : now.month==1 and now.day==1
     })
